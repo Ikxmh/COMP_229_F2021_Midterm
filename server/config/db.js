1992fedf0ccf8966b470fb4f2a3fba199772bf35
Ikamjot Hundal <ihundal@my.centennialcollege.ca>
2021-10-24 21:05:28 -0400

    updated DB uri to work with MongoDB Atlas

--- server/config/db.js
+++ server/config/db.js
@@ -1,4 +1,5 @@
 module.exports = {
   //local MongoDB deployment ->
-  "URI": "mongodb://localhost/book_library"
+  //"URI": "mongodb://localhost/book_library"
+  "URI": "mongodb+srv://ikamjot:28oohfgBLbfN7CEf@mongodbserver.pav8s.mongodb.net/book_library?retryWrites=true&w=majority"
 };
